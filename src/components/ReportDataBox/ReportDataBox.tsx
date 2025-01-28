2f9b5477867527e9903c00616a021aca5efe98e9
cesarsroy <cesarsroy@gmail.com>
2025-01-28 20:50:33 +0800

    got reportData endpoint to work and parse the json string into objects

--- src/components/ReportDataBox/ReportDataBox.tsx
+++ src/components/ReportDataBox/ReportDataBox.tsx
@@ -1,48 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import {useForm} from 'react-hook-form';
-import styles from './ReportCatalogBox.module.css'
-import getReportData, { ReportFormData } from '../../services/reportData';
-import { Axios, AxiosError } from 'axios';
-import { InformationObject } from '../../services/reportCatalog';
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import styles from "./ReportCatalogBox.module.css";
+import getReportData, { ReportFormData } from "../../services/reportData";
+import { Axios, AxiosError } from "axios";
+import { InformationObject } from "../../services/reportCatalog";
 
 interface Props {
-    accessToken: string,
-    reportCatalog: InformationObject[],
+  accessToken: string;
+  reportCatalog: InformationObject[];
 }
 
-const ReportDataBox = ({accessToken, reportCatalog} : Props) => {
-    
-    const {register, formState, handleSubmit} = useForm();
+const ReportDataBox = ({ accessToken, reportCatalog }: Props) => {
+  const { register, formState, handleSubmit } = useForm();
 
-    const callData =
-        ()=>{
-            const body = {
-                reportInformation: {
-                    portfolio:'POAPI_ALM_DEMO',
-                    reportName:'POPAPI_DEMO_TE'
+  const callData = () => {
+    const body = {
+      reportInformation: {
+        portfolio: "POPAPI_ALM_DEMO",
+        reportName: "POPAPI_DEMO_TE",
+      },
+    };
+    console.log("Token before data", accessToken);
+    getReportData(body, accessToken).then((d) => console.log(d));
+  };
 
-                }
-                
-            }
-            console.log('Token before data',accessToken)
-            getReportData(body,accessToken).then((d)=>console.log(d))
-        }
-
-    return (
-        <div className="h4 text-secondary mt-3 pt-2 border-top border-secondary">
-        Report Data Example
-        <p className="fst-italic fw-normal h6 pt-2">Use this to retrieve the actual report data.</p>
-        <button className='btn btn-success' onClick={()=>callData()}>Run</button>
-
-
+  return (
+    <div className="h4 text-secondary mt-3 pt-2 border-top border-secondary">
+      Report Data Example
+      <p className="fst-italic fw-normal h6 pt-2">
+        Use this to retrieve the actual report data.
+      </p>
+      <button className="btn btn-success" onClick={() => callData()}>
+        Run
+      </button>
     </div>
+  );
+};
 
-    )
-
-    
-
-}
-
-    
-
-export default ReportDataBox
+export default ReportDataBox;
--- src/services/reportData.ts
+++ src/services/reportData.ts
@@ -8,17 +8,49 @@ export interface ReportFormData{
 
     }
 
+function parseMultiPartData(responseString:string) {
+    const regex = /\{.*\}/g; 
+    const matches = responseString.match(regex) || []; 
+    return matches
+
+}
+
+interface DataColumn {
+    [key: string]: any;
+    name: string;
+    type: string;
+    doubles:number[];
+    dates: Date[];
+    strings:string[];
+}
+
+function toDataRecords(dataColumns:DataColumn[] ) {
+    // transforms the report data response into list of records
+    let i = 0
+    const firstColArray = dataColumns[0][dataColumns[0].type + 's']
+    const dataLength = firstColArray.length
+    const records = new Array()
+    while (i < dataLength) {
+        const record =  {} as {[key:string]:any}
+        for (const col of dataColumns) {
+            record[col.name] = col[col.type +'s'][i]
+        }
+        records.push(record)
+        i++
+        
+    }
+    return records
+
+}
 
 async function getReportData(body: ReportFormData , 
                             accessToken: string | null
                         ) {
     
-    if (accessToken == null) {
+    if (!accessToken) {
         throw new Error('You need an access token.')
     }
     const dataPath = 'https://api.bloomberg.com/enterprise/portfolio/report/data'
-
-    const data: string = JSON.stringify(body)
 
     const headers = {
         "Content-Type": "application/json",
@@ -26,17 +58,19 @@ async function getReportData(body: ReportFormData ,
     };
 
     console.info('GET Request sent with this data and headers')
-    console.info(data)
+    console.info(body)
     console.info(headers)
     const dataResponse = await axios.post(dataPath,
-        { headers ,
-            body,
-            
-        }
+        body,
+        {headers}
 
         ).then((res) => {
-            console.log(`response:`)
+            console.log(`Raw report data response:`)
             console.log(res)
+            const jsonStrings = parseMultiPartData(res.data)
+            const records = jsonStrings.map(s=> toDataRecords(JSON.parse(s).dataColumns))
+            const allRecords = records.reduce((acc, arr) => acc.concat(arr),[])
+            return allRecords
 
         }).catch(err => {
             console.log('Error occurred')
