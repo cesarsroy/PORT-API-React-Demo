08dfe02c119286c38d30a7f4d37f71f2ab9d4024
cesarsroy <74352387+cesarsroy@users.noreply.github.com>
2025-02-14 07:13:57 +0800

    removed credentials

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 
-export const serverclientId = 'd83dec8af437da88ee4fc539eecfa4a2';
-export const serverClientSecret= 'c884e3042d2e888bfbda29a0a41d514c5e3f8816a5c1afd4b4354c61fbc067dd';
+export const serverclientId = '';
+export const serverClientSecret= '';
